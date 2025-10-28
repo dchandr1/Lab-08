61265b77fbd3d0b67fe74953ce058befd4683780
dchandr1 <dchandr1@ualberta.ca>
2025-10-28 17:41:53 -0600

    Test adding comment

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -15,7 +15,7 @@ public class CustomList {
     }
 
     
-
+    // added a comment
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
 }
